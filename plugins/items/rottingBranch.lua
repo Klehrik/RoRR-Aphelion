d76d1b74a8d2fd58663933f5940f537ee073fe81
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-08-08 23:45:03 -0400

    branch adjustment

--- plugins/items/rottingBranch.lua
+++ plugins/items/rottingBranch.lua
@@ -31,19 +31,19 @@ Buff.set_property(buff, Buff.PROPERTY.is_debuff, true)
 
 Buff.add_callback(buff, "onApply", function(actor, stack)
     if (not actor.aphelion_rottingBranch_timer) or stack == 1 then actor.aphelion_rottingBranch_timer = 0 end
-    actor.aphelion_rottingBranch_duration = math.ceil(210.0 / math.max(stack * 0.333, 1.0))
+    actor.aphelion_rottingBranch_duration = math.ceil(210.0 / math.max(stack * 0.4, 1.0))
 end)
 
 Buff.add_callback(buff, "onStep", function(actor, stack)
     actor.aphelion_rottingBranch_timer = actor.aphelion_rottingBranch_timer + 1
     
     if gm.instance_exists(actor.aphelion_rottingBranch_attacker) and actor.aphelion_rottingBranch_timer % 30 == 0 then
-        Actor.damage(actor, actor.aphelion_rottingBranch_attacker, actor.aphelion_rottingBranch_attacker.damage * 0.12 * stack, actor.x - 26, actor.y - 36, 7964834)
+        Actor.damage(actor, actor.aphelion_rottingBranch_attacker, actor.aphelion_rottingBranch_attacker.damage * 0.15 * stack, actor.x - 26, actor.y - 36, 7964834)
     end
 
     actor.aphelion_rottingBranch_duration = actor.aphelion_rottingBranch_duration - 1
     if actor.aphelion_rottingBranch_duration <= 0 then
         Buff.remove(actor, Buff.find("aphelion-rottingBranch"), 1)
-        actor.aphelion_rottingBranch_duration = math.ceil(210.0 / math.max((stack - 1) * 0.333, 1.0))
+        actor.aphelion_rottingBranch_duration = math.ceil(210.0 / math.max((stack - 1) * 0.4, 1.0))
     end
 end)
