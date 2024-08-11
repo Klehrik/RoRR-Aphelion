94a7c467ae9b765589e10e15e468b3fc43c1aeaf
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-08-11 13:41:07 -0400

    s shield

--- plugins/items/superShield.lua
+++ plugins/items/superShield.lua
@@ -8,15 +8,29 @@ Item.set_tier(item, Item.TIER.rare)
 Item.set_loot_tags(item, Item.LOOT_TAG.category_healing)
 
 Item.add_callback(item, "onPickup", function(actor, stack)
-    actor.aphelion_superShield_increase = 0.0
+    if not actor.aphelion_superShield_increase then actor.aphelion_superShield_increase = 0.0 end
 end)
 
--- Item.add_callback(item, "onRemove", function(actor, stack)
---     local increase = 20
---     if stack > 1 then increase = 15 end
---     actor.maxshield_base = actor.maxshield_base - increase
--- end)
+Item.add_callback(item, "onRemove", function(actor, stack)
+    if stack == 1 then
+        actor.maxshield_base = actor.maxshield_base - actor.aphelion_superShield_increase
+        actor.maxshield = actor.maxshield - actor.aphelion_superShield_increase
+        actor.aphelion_superShield_increase = nil
+    end
+end)
 
 Item.add_callback(item, "onStep", function(actor, stack)
+    local goal = actor.maxhp * Helper.mixed_hyperbolic(stack, 0.15, 0.2)
+
+    if actor.aphelion_superShield_increase ~= goal then
+        local diff = goal - actor.aphelion_superShield_increase
+        actor.aphelion_superShield_increase = goal
+
+        actor.maxshield_base = actor.maxshield_base + diff
+        actor.maxshield = actor.maxshield + diff
+    end
+end)
+
+Item.add_callback(item, "onShieldBreak", function(actor, stack)
     
 end)
