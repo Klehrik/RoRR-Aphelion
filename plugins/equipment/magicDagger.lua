f251ac6712cc360678b1a5f8ce8dd4fa4a97d0a9
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-08-17 01:30:21 -0400

    Mdagger

--- plugins/equipment/magicDagger.lua
+++ plugins/equipment/magicDagger.lua
@@ -8,7 +8,7 @@ Equipment.set_cooldown(equip, 45)
 Equipment.set_loot_tags(equip, Item.LOOT_TAG.category_damage, Item.LOOT_TAG.category_utility)
 
 Equipment.add_callback(equip, "onUse", function(actor)
-    local inst = Object.spawn(Object.find("aphelion", "magicDaggerFreeze"), actor.x + (24 * gm.sign(actor.image_xscale)), actor.bbox_bottom + 1)
+    local inst = Object.spawn(Object.find("aphelion", "magicDaggerFreeze"), actor.x + (32 * gm.sign(actor.image_xscale)), actor.bbox_bottom + 1)
     inst.parent = actor
     inst.image_xscale = inst.image_xscale * gm.sign(actor.image_xscale)
 end)
@@ -24,9 +24,10 @@ local obj = Object.create("aphelion", "magicDaggerFreeze")
 Object.add_callback(obj, "Init", function(self)
     self.sprite_index = sprite
     self.image_index = 0
-    self.image_speed = 0.18
-    
-    self.image_xscale = 3.0
+    self.image_speed = 0.15
+
+    self.image_alpha = 0.85
+    self.image_xscale = 4.0
     self.image_yscale = 2.0
 
     self.damage_coeff = 3.0
@@ -38,10 +39,10 @@ Object.add_callback(obj, "Step", function(self)
     if (not self.hit) and self.image_index >= 1.5 then
         self.hit = true
 
-        local radius_x = 29 * self.image_xscale
-        local radius_y = 23 * self.image_yscale
+        local radius_x = 29 * math.abs(self.image_xscale)
+        local radius_y = 23 * math.abs(self.image_yscale)
 
-        local damager = Actor.fire_explosion(self.parent, self.x + (radius_x * gm.sign(self.image_xscale)), self.y, radius_x, radius_y, self.damage_coeff, self.freeze_time + gm.random_range(0, 0.5))
+        local damager = Actor.fire_explosion(self.parent, self.x + (radius_x * gm.sign(self.image_xscale)), self.y - radius_y, radius_x, radius_y, self.damage_coeff, self.freeze_time + gm.random_range(0, 0.5))
         damager.knockback_kind = 3
         damager.damage_color = 14064784
     end
