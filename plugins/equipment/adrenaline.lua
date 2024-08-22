7bff56d88a4411450452088450be341c9f1c08dc
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-08-21 23:03:16 -0400

    Stimulants buff

--- plugins/equipment/adrenaline.lua
+++ plugins/equipment/adrenaline.lua
@@ -11,4 +11,25 @@ Equipment.add_callback(equip, "onUse", function(actor)
     Actor.add_barrier(actor, actor.maxbarrier * 0.75)
     if actor.invincible == false then actor.invincible = 0 end
     actor.invincible = math.max(actor.invincible, 1.2 *60)
+    Buff.apply(actor, Buff.find("aphelion-adrenaline"), 5 *60)
+end)
+
+
+
+-- Buff
+
+local increase = 2.8 * 0.4
+
+local sprite = Resources.sprite_load(PATH.."assets/sprites/buffAdrenaline.png", 1, false, false, 7, 5)
+
+local buff = Buff.create("aphelion", "adrenaline")
+Buff.set_property(buff, Buff.PROPERTY.icon_sprite, sprite)
+Buff.set_property(buff, Buff.PROPERTY.icon_stack_subimage, false)
+
+Buff.add_callback(buff, "onApply", function(actor, stack)
+    actor.pHmax_base = actor.pHmax_base + increase
+end)
+
+Buff.add_callback(buff, "onRemove", function(actor, stack)
+    actor.pHmax_base = actor.pHmax_base - increase
 end)
