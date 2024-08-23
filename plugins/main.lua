31a556c3c31250287086209e1ff222fab34d8b8a
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-08-23 18:39:55 -0400

    Update main.lua

--- plugins/main.lua
+++ plugins/main.lua
@@ -3,6 +3,8 @@
 log.info("Successfully loaded ".._ENV["!guid"]..".")
 mods.on_all_mods_loaded(function() for _, m in pairs(mods) do if type(m) == "table" and m.RoRR_Modding_Toolkit then Actor = m.Actor Buff = m.Buff Callback = m.Callback Equipment = m.Equipment Helper = m.Helper Instance = m.Instance Item = m.Item Net = m.Net Object = m.Object Player = m.Player Resources = m.Resources Survivor = m.Survivor break end end end)
 
+Aphelion = true
+
 PATH = _ENV["!plugins_mod_folder_path"].."/plugins/"
 -- Remove "/plugins" from PATH when uploading to Thunderstore
 
