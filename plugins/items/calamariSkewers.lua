9aafbe91ddbda20a4dfd78f5792c1575ae5c42c9
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-09-08 11:58:58 -0400

    New actor:get_data() usage

--- plugins/items/calamariSkewers.lua
+++ plugins/items/calamariSkewers.lua
@@ -8,19 +8,22 @@ item:set_tier(Item.TIER.common)
 item:set_loot_tags(Item.LOOT_TAG.category_healing)
 
 item:add_callback("onPickup", function(actor, stack)
-    actor.aphelion_calamariSkewers_cooldown = 0
-    if not actor.aphelion_calamariSkewers_timers then actor.aphelion_calamariSkewers_timers = Array.new() end
+    local actor_data = actor:get_data("aphelion-calamariSkewers")
+    actor_data.cooldown = 0
+    if not actor_data.timers then actor_data.timers = {} end
 end)
 
 item:add_callback("onKill", function(actor, victim, damager, stack)
-    if actor.aphelion_calamariSkewers_cooldown <= 0 then
+    local actor_data = actor:get_data("aphelion-calamariSkewers")
+    if actor_data.cooldown <= 0 then
         actor:buff_apply(Buff.find("aphelion-calamariSkewers"), 1)
     end
 end)
 
 item:add_callback("onStep", function(actor, stack)
-    if actor.aphelion_calamariSkewers_cooldown > 0 then
-        actor.aphelion_calamariSkewers_cooldown = actor.aphelion_calamariSkewers_cooldown - 1
+    local actor_data = actor:get_data("aphelion-calamariSkewers")
+    if actor_data.cooldown > 0 then
+        actor_data.cooldown = actor_data.cooldown - 1
     end
 end)
 
@@ -38,30 +41,28 @@ buff.max_stack = 4
 buff.is_timed = false
 
 buff:onApply(function(actor, stack)
-    actor.aphelion_calamariSkewers_timers:push(60)
+    local actor_data = actor:get_data("aphelion-calamariSkewers")
+    table.insert(actor_data.timers, 60)
 end)
 
 buff:onStep(function(actor, stack)
-    -- Decrease stack timers
-    local array = actor.aphelion_calamariSkewers_timers
-    for i, time in ipairs(array) do
-        time = time - 1
-        array[i] = time
-    end
+    local actor_data = actor:get_data("aphelion-calamariSkewers")
 
-    -- Remove oldest stack if expired
-    local time = array:get(0)
-    if time and time <= 0 then array:delete(0) end
+    -- Decrease stack timers
+    -- and remove if expired
+    for i, time in ipairs(actor_data.timers) do
+        actor_data.timers[i] = time - 1
+        if time <= 0 then table.remove(actor_data.timers, i) end
+    end
 
     -- Check if 4 kills within 1 second has been achieved
-    local size = array:size()
-    if size >= 4 then
-        local item_stack = actor:item_stack_count(Item.find("aphelion-calamariSkewers"))
+    if #actor_data.timers >= 4 then
+        local item_stack = actor:item_stack_count(item)
         actor:heal((item_stack * 20) + ((actor.maxhp - actor.hp) * 0.1))
-        actor.aphelion_calamariSkewers_cooldown = 2 *60
-        array:clear()
+        actor_data.cooldown = 2 *60
+        actor_data.timers = {}
     end
 
-    -- Remove buff stacks if more than ds_list size
-    if stack > size then actor:buff_remove(buff, stack - size) end
+    -- Remove buff stacks if more than table size
+    if stack > #actor_data.timers then actor:buff_remove(buff, stack - #actor_data.timers) end
 end)
