9a9ebf4c4aaab432d5187de04ac61578eb590b43
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-10-16 16:32:34 -0400

    Update misc.lua

--- misc.lua
+++ misc.lua
@@ -4,6 +4,6 @@
 Skill.find("aphelion-huntressStealth").upgrade_skill = Skill.find("aphelion-huntressStealthBoosted")
 
 -- Huntress : Grant 15 i-frames on Blink
-Player:onSkillUse("aphelion-huntressBlinkTweak", function(actor)
+Skill.find("ror-huntressC"):onActivate(function(actor, skill, index)
     actor:set_immune(15)
-end, Skill.find("ror-huntressC"))
+end)
