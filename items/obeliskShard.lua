6e86a32b0409f4a9cb668d4af03f78ee3dae65f8
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-12-04 22:29:50 -0500

    Update obeliskShard.lua

--- items/obeliskShard.lua
+++ items/obeliskShard.lua
@@ -33,10 +33,10 @@ local part2 = Particle.new("aphelion", "pray2")
 for _, p in ipairs({part, part2}) do
     p:set_sprite(sprite, false, false, false)
     p:set_direction(90, 90, 0, 0)
-    p:set_speed(0.1, 0.2, 1/40, 0)
+    p:set_speed(0.08, 0.15, 1/60, 0)
     p:set_color_mix(Color.WHITE, blue)
     p:set_alpha3(1, 0.8, 0)
-    p:set_life(35, 45)
+    p:set_life(40, 50)
 end
 part:set_orientation(25, 45, 1, 0.5, false)
 part2:set_orientation(-25, -45, -1, 0.5, false)
@@ -46,8 +46,20 @@ item:onPostDraw(function(actor, stack)
     if gm.variable_global_get("pause") then return end
     
     if actor.still_timer >= 2 *60.0 then
-        if Helper.chance(0.04) then part:create(actor.x + gm.random_range(-4, 10), actor.y + 6, 1, Particle.SYSTEM.above) end
-        if Helper.chance(0.04) then part2:create(actor.x + gm.random_range(-10, 4), actor.y + 6, 1, Particle.SYSTEM.below) end
+        local actorData = actor:get_data("pray")
+        if not actorData.part then actorData.part = 0 end
+        if not actorData.part2 then actorData.part2 = 12 end
+
+        if actorData.part > 0 then actorData.part = actorData.part - 1
+        else
+            actorData.part = gm.irandom_range(15, 25)
+            part:create(actor.x + gm.random_range(-4, 8), actor.y + 6, 1, Particle.SYSTEM.above)
+        end
+        if actorData.part2 > 0 then actorData.part2 = actorData.part2 - 1
+        else
+            actorData.part2 = gm.irandom_range(15, 25)
+            part2:create(actor.x + gm.random_range(-8, 4), actor.y + 6, 1, Particle.SYSTEM.middle)
+        end
 
         if not loops[actor.id] then
             loops[actor.id] = gm.sound_loop(sound, 0.9)   -- arg2 is volume
