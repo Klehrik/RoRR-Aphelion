69d21babc277a2d199d8f385dba6758b6b0e5fe5
Klehrik <78520710+Klehrik@users.noreply.github.com>
2026-02-10 00:39:09 -0500

    Add Six Shooter

---
+++ items/sixShooter.lua
@@ -0,0 +1,189 @@
+-- Six Shooter
+
+local item = Item.new("sixShooter")
+local packet
+
+
+-- ===== Assets =====
+
+local sprite = Sprite.new("item/sixShooter", "~/assets/sprites/items/sixShooter.png", 1, 16, 17)
+
+
+-- ===== Properties =====
+
+item:set_sprite(sprite)
+item:set_tier(ItemTier.UNCOMMON)
+item.loot_tags = Item.LootTag.CATEGORY_DAMAGE
+
+local fade_speed   = 6      -- Time (in frames) to fully fade-in/out the display
+local damage_bonus = 0.33   -- 6th shot damage bonus per stack
+
+
+-- ===== Functions =====
+
+local increment_and_rotate = function(actor_data)
+    actor_data.count = actor_data.count + 1
+    actor_data.rotation_to = actor_data.rotation_to + 60
+    actor_data.visible:start()
+
+    if actor_data.count >= 6 then
+        actor_data.count = actor_data.count - 6
+        actor_data.ready = true
+        actor_data.flash:start()
+    end
+end
+
+
+-- ===== Callbacks =====
+
+Callback.add(item.on_acquired, function(actor, stack)
+    local actor_data = Instance.get_data(actor, "sixShooter")
+    actor_data.count       = 0
+    actor_data.ready       = false
+    actor_data.rotation    = 90
+    actor_data.rotation_to = 90
+    actor_data.alpha       = 0
+    actor_data.visible     = Timer(120)
+    actor_data.flash       = Timer(20)
+    actor_data.surface     = -1
+end)
+
+
+Callback.add(Callback.ON_SKILL_ACTIVATE, function(actor, slot)
+    -- Check item count
+    local stack = actor:item_count(item)
+    if stack <= 0 then return end
+
+    if slot == Skill.Slot.PRIMARY then
+        local actor_data = Instance.get_data(actor, "sixShooter")
+        actor_data.primary_use = true
+    end
+end)
+
+
+Callback.add(Callback.ON_ATTACK_CREATE, function(attack_info)
+    -- Check if this is a procing attack
+    if not Util.bool(attack_info.proc) then return end
+
+    local actor = attack_info.parent
+    if not Instance.exists(actor) then return end
+
+    -- Check item count
+    local stack = actor:item_count(item)
+    if stack <= 0 then return end
+
+    local actor_data = Instance.get_data(actor, "sixShooter")
+
+    -- Check if primary skill has been used
+    -- Not 100% accurate but works fine for the most part
+    if actor_data.primary_use then
+        actor_data.primary_use = nil
+
+        -- Increment and start rotation
+        increment_and_rotate(actor_data)
+        packet:send_to_all(actor)
+    end
+
+    -- Check if 6th shot has been fired
+    if actor_data.ready then
+        actor_data.ready = false
+
+        -- Apply damage bonus and guaranteed crit
+        attack_info:set_damage(attack_info:get_damage_nocrit() * (1 + (damage_bonus * stack)))
+        attack_info:set_critical(true)
+    end
+end)
+
+
+Callback.add(Callback.ON_DRAW, function()
+    local actors = item:get_holding_actors()
+
+    for _, actor in ipairs(actors) do
+        local actor_data = Instance.get_data(actor, "sixShooter")
+
+        -- Fade-in/out
+        local dir = math.sign(actor_data.visible.time_left)
+        actor_data.alpha = math.clamp(actor_data.alpha + dir/fade_speed, 0, 1)
+
+        -- Rotate
+        local delta = actor_data.rotation_to - actor_data.rotation
+        local speed_div = 8
+        if delta < 1 then
+            actor_data.rotation = actor_data.rotation_to
+        else
+            actor_data.rotation = actor_data.rotation + delta/speed_div
+        end
+
+        -- Create surface if existn't
+        if not Util.bool(gm.surface_exists(actor_data.surface)) then
+            actor_data.surface = gm.surface_create(49, 49)
+        end
+        gm.surface_set_target(actor_data.surface)
+
+        -- Cylinder body
+        gm.draw_circle(24, 24, 24, false)
+
+        gm.gpu_set_blendmode(3)
+
+        -- Center
+        gm.draw_circle(24, 24, 4, false)
+
+        local v = Vector.UP
+        v.direction = actor_data.rotation
+
+        for i = 1, 6 do
+            -- Cylinder
+            local offset = 15
+
+            -- Loaded round
+            if actor_data.count < i then
+                for r = 5, 6, 0.5 do
+                    gm.draw_circle(24 + v.x * offset, 24 + v.y * offset, r, true)
+                end
+                gm.draw_circle(24 + v.x * offset, 24 + v.y * offset, 1, false)
+
+            -- Spent casing
+            else
+                gm.draw_circle(24 + v.x * offset, 24 + v.y * offset, 6, false)
+
+            end
+            v.direction = v.direction - 30
+
+            -- Groove
+            local offset = 28
+            gm.draw_circle(24 + v.x * offset, 24 + v.y * offset, 6, false)
+            v.direction = v.direction - 30
+        end
+
+        gm.gpu_set_blendmode(0)
+        gm.surface_reset_target()
+
+        -- Draw surface
+        local flash = math.max(actor_data.flash.time_left / actor_data.flash.duration, 0)
+        local color = gm.merge_color(Color.WHITE, Color.YELLOW, flash)
+        gm.draw_surface_ext(actor_data.surface, actor.x - 24, actor.y - 66, 1, 1, 0, color, (actor_data.alpha * 0.4) + (flash * 0.6))
+    end
+end)
+
+
+-- ===== Packets =====
+
+packet = Packet.new("sixShooter")
+packet:set_serializers(
+    function(buffer, actor)
+        buffer:write_instance(actor)
+    end,
+
+    function(buffer, player)
+        local actor = buffer:read_instance()
+        if Instance.exists(actor) then
+            local actor_data = Instance.get_data(actor, "sixShooter")
+            increment_and_rotate(actor_data)
+        end
+    end
+)
+
+
+-- ===== Additional =====
+
+ItemLog.new_from_item(item)
--- language/english.lua
+++ language/english.lua
@@ -33,7 +33,15 @@ return {
             destination = "Complex 3B,\nSaturn,\n???",
             date        = "6/4/2056",
             story       = "...?\n\nBOOTING...\n\nSCANNING SURROUNDINGS\n\nUNKNOWN PRESCENCE DETECTED\n\n...\n\nFAILED TO LOAD DIRECTIVES\n\n...\n\nRESETTING...\n\nESTABLISHING NEW DIRECTIVES\n\n...\n\nESCORTING FRIENDLY LIFEFORM",
-            priority    = "<g>Field-found</c>"
+            priority    = "<g>Field-found</c>",
+        },
+        sixShooter = {
+            name        = "Six Shooter",
+            pickup      = "Every 6 basic attacks critically strike.",
+            description = "Every <b>6th basic attack</c> deals <y>33% <c_stack>(+33% per stack) <y>more damage</c> and is <y>guaranteed to critically strike</c>.",
+            destination = "Apt. #302,\n12th District,\nNeo Metro,\nVenus",
+            date        = "8/13/2056",
+            story       = "An antique from days past. The cylinder is somewhat damaged, and black powder is pretty hard (and expensive!) to get nowadays, but regardless it should be a nice addition to your collection if you aren't planning on firing it.",
         },
 
 
